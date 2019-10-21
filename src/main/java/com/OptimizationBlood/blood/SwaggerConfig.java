37dc783022b831f2a1d0e925519116d3748e93e0
FaruqueBraimo <faruquebraimo@gmail.com>
2019-10-21 11:17:57 -0700

    hh
    hh

--- src/main/java/com/OptimizationBlood/blood/SwaggerConfig.java
+++ src/main/java/com/OptimizationBlood/blood/SwaggerConfig.java
@@ -1,46 +1,49 @@
-package com.OptimizationBlood.blood;
+package com.produtos.apirest.config;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
-    public Docket greetingApi() {
+    public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.swaggerready"))
+                .apis(RequestHandlerSelectors.basePackage("com.Optimization.Blood.blood"))
+                .paths(regex("/sangue.*"))
                 .build()
-                .apiInfo(metaData());
-
+                .apiInfo(metaInfo());
     }
 
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for greeting people\"")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .build();
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Michelli Brito", "https://www.youtube.com/michellibrito",
+                        "michellidibrito@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
     }
 
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 }
