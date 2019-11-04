21d01b4f8f0ca9cc64c9a5bdcbeb04936256c4f5
FBraimo <faruquebraimo@gmail.com>
2019-11-04 09:09:51 -0800

    Update DoacaoResource.java

--- src/main/java/com/OptimizationBlood/blood/resources/DoacaoResource.java
+++ src/main/java/com/OptimizationBlood/blood/resources/DoacaoResource.java
@@ -94,7 +94,7 @@ public class DoacaoResource {
 
 
 
-        return "ups";
+        return "sucesso";
     }
 
     @GetMapping("/doacoes")
@@ -106,17 +106,6 @@ public class DoacaoResource {
 
     }
 
-    @GetMapping("doacao/{codigo}")
-    @ApiOperation(value="retorna uma certa doacao")
-
-    public Doacao pesquisar(@PathVariable(value = "codigo") int codigo){
-
-        return dr.findByCodigo(codigo);
-
-
-
-
-    }
 
 
     @DeleteMapping("/doacao")
@@ -135,20 +124,8 @@ public class DoacaoResource {
 
     }
 
-    @PutMapping("/doacao")
-    @ApiOperation(value="Edita uma certa doacao")
-    public  Doacao editar(@RequestBody Doacao doacao){
-        return dr.save(doacao);
+  
 
-    }
-
-    @GetMapping("dadordoacao/{codigo}")
-    @ApiOperation(value="retorna uma  certa doacao")
-
-    public Doacao pesquisarAgendamento(@PathVariable(value = "codigo") int id){
-
-        return dr.doacao(id);
-
-    }
+   
 
 }
