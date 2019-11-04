5a442128229bdbab52ca5e1ded96a3821cc2450a
FaruqueBraimo <faruquebraimo@gmail.com>
2019-11-04 06:18:51 -0800

    list

--- src/main/java/com/OptimizationBlood/blood/repository/AgendamentoRepository.java
+++ src/main/java/com/OptimizationBlood/blood/repository/AgendamentoRepository.java
@@ -3,6 +3,8 @@ package com.OptimizationBlood.blood.repository;
 import com.OptimizationBlood.blood.models.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
 
 public interface AgendamentoRepository  extends JpaRepository<Agendamento,Integer> {
 
